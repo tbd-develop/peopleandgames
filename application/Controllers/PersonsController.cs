12d30428a07e774e81fcb305c586c9e7a87b0f26
Terry Burns-Dyson <terrybd@gmail.com>
2019-05-01 13:25:37 -0400

    Added model for person, included litedb and added person to db

---
+++ application/Controllers/PersonsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using application.Infrastructure.Inputs;
+using application.Infrastructure.Models;
+using LiteDB;
+using Microsoft.AspNetCore.Mvc;
+
+namespace application.Controllers
+{
+    public class PersonsController : Controller
+    {
+        [HttpPost, Route("person")]
+        public void AddPerson([FromBody]PersonInputModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new LiteDatabase(@"MyData.db"))
+                {
+                    // Get person collection
+                    var persons = db.GetCollection<Person>("persons");
+
+                    // Create your new person instance
+                    var person = new Person
+                    {
+                        FirstName = model.FirstName,
+                        LastName = model.LastName,
+                        Email = model.Email,
+                        Phone = model.Phone
+                    };
+
+                    // Insert new person document (Id will be auto-incremented)
+                    persons.Insert(person);
+
+                    // Index document using a document property
+                    persons.EnsureIndex(x => x.LastName);
+                }
+            }
+        }
+    }
+}
---
+++ application/Infrastructure/Inputs/PersonInputModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace application.Infrastructure.Inputs
+{
+    public class PersonInputModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
