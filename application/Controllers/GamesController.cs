b027d30aeb0e46ca8c585e090f625c6473367bc8
Terry Burns-Dyson <terrybd@gmail.com>
2019-05-01 20:54:09 -0400

    Adding a game into the database for a person

--- application/Controllers/GamesController.cs
+++ application/Controllers/GamesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using application.Infrastructure.Inputs;
+using application.Infrastructure.Models;
+using LiteDB;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,6 +17,36 @@ namespace application.Controllers
         {
             if (ModelState.IsValid)
             {
+                using (var db = new LiteDatabase(@"MyData.db"))
+                {
+                    // Get games collection
+                    var games = db.GetCollection<Game>("games");
+
+                    var existsAlready = games.FindOne(f =>
+                        f.PersonId == model.PersonId &&
+                        f.Name.Equals(model.Name, StringComparison.CurrentCultureIgnoreCase));
+
+                    if (existsAlready != null)
+                    {
+                        return StatusCode(StatusCodes.Status409Conflict);
+                    }
+
+                    // Create your new game instance
+                    var game = new Game
+                    {
+                        PersonId = model.PersonId,
+                        Name = model.Name,
+                        Platform= model.Platform,
+                        Year = model.Year
+                    };
+
+                    // Insert new game document (Id will be auto-incremented)
+                    games.Insert(game);
+
+                    games.EnsureIndex(x => x.PersonId);
+                    games.EnsureIndex(x => x.Platform);
+                }
+
                 return StatusCode(StatusCodes.Status200OK);
             }
 
---
+++ application/Infrastructure/Models/Game.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace application.Infrastructure.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public int PersonId { get; set; }
+        public string Name { get; set; }
+        public string Platform { get; set; }
+        public int Year { get; set; }
+    }
+}
